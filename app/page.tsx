c20d5c7d8e69714a7505f801aeca595edb721a7e
VojtaKaniok11 <vojtikkaniok@seznam.cz>
2026-01-29 17:36:55 +0100

    ostranil jsem tlačítko

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { TodoList } from '@/components/todo-list';
-import { MyButton } from '@/components/ui/MyButton';
 import { use } from 'react';
 
 export default function Home() {
@@ -14,9 +13,6 @@ export default function Home() {
         <h1 className='text-3xl font-bold text-center mb-8 text-foreground'>
           Můj TO-DO List
         </h1>
-        <div className='flex justify-center mb-8'>
-          <MyButton label='Moje nové tlačítko' onClick={testuj} color='blue' />
-        </div>
         <TodoList />
       </div>
     </main>
